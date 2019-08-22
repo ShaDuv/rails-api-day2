527a52287e6496b98ebc524ff5488b770c24346d
BenB-C <bcauchon@gmail.com>
2019-08-22 10:09:30 -0700

    add specs for the remaining place routes
    Co-Authored-By: ShaDuv <Shawnabduvall@gmail.com>

--- spec/requests/places_routes_spec.rb
+++ spec/requests/places_routes_spec.rb
@@ -16,29 +16,26 @@ describe 'get places route', type: :request do
 end
 
 describe 'post places route', type: :request do
-  place_name = "Eifel Tower"
-  place_city = "Paris"
-  place_country = "France"
+  new_name = "Eifel Tower"
+  new_city = "Paris"
+  new_country = "France"
+
   before do
     Place.destroy_all
-    post '/places', params: { name: place_name, city: place_city, country: place_country }
+    post '/places', params: { name: new_name, city: new_city, country: new_country }
   end
 
   it 'creates a new place' do
     expect(Place.count).to eq(1)
     place = Place.all[0]
-    expect(place.name).to eq(place_name)
-    expect(place.city).to eq(place_city)
-    expect(place.country).to eq(place_country)
+    expect(place.name).to eq(new_name)
+    expect(place.city).to eq(new_city)
+    expect(place.country).to eq(new_country)
   end
 
   it 'returns the created place' do
-    body = JSON.parse(response.body)
     place = Place.all[0]
-    expect(body["id"]).to eq(place.id)
-    expect(body["name"]).to eq(place.name)
-    expect(body["city"]).to eq(place.city)
-    expect(body["country"]).to eq(place.country)
+    expect_json_to_eq_object(response, place)
   end
 end
 
@@ -47,10 +44,48 @@ describe 'get place route', type: :request do
     place = FactoryBot.create(:place)
 
     get "/places/#{place.id}"
-    body = JSON.parse(response.body)
-    expect(body["id"]).to eq(place.id)
-    expect(body["name"]).to eq(place.name)
-    expect(body["city"]).to eq(place.city)
-    expect(body["country"]).to eq(place.country)
+    expect_json_to_eq_object(response, place)
   end
+end
+
+describe 'patch place route', type: :request do
+  it 'updates the place with the given id using the given parameters' do
+    place = FactoryBot.create(:place)
+    new_name = "Eifel Tower"
+    new_city = "Paris"
+    new_country = "France"
+    patch "/places/#{place.id}", params: { name: new_name, city: new_city, country: new_country }
+    place = Place.find(place.id)
+    expect(place.name).to eq(new_name)
+    expect(place.city).to eq(new_city)
+    expect(place.country).to eq(new_country)
+  end
+end
+
+describe 'patch place route with missing parameter', type: :request do
+  it 'updates the place with the given id using the given parameters' do
+    place = FactoryBot.create(:place)
+    new_name = "Eifel Tower"
+    new_city = "Paris"
+    patch "/places/#{place.id}", params: { name: new_name, city: new_city, country: "" }
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
+end
+
+describe 'delete place route', type: :request do
+  it 'destroys the place with the given id' do
+    place = FactoryBot.create(:place)
+    delete "/places/#{place.id}"
+    expect(response).to have_http_status(:success)
+    get "/places/#{place.id}"
+    expect(response).to have_http_status(:not_found)
+  end
+end
+
+def expect_json_to_eq_object(json_response, place_object)
+  body = JSON.parse(json_response.body)
+  expect(body["id"]).to eq(place_object.id)
+  expect(body["name"]).to eq(place_object.name)
+  expect(body["city"]).to eq(place_object.city)
+  expect(body["country"]).to eq(place_object.country)
 end
